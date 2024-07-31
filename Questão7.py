0e91a405122428fd2c78108605ace61dd4f59878
Yan F maia <31291355+yanmaia@users.noreply.github.com>
2024-07-31 10:12:30 -0300

    Update Questão7.py

--- Questão7.py
+++ Questão7.py
@@ -3,29 +3,28 @@ vinicial=int(input('Informe um valor inteiro inicial da PA: '))
 razaopa=float(input('Informe a Razao da PA: '))
 vquantidade=int(input('Informe a quantidade de elementos positivo de uma  PA: '))
 valorpa=vinicial
-valorpa2=valorpa
-soma=0
-lista=' '
-#if vquantidade<0:           #Verifica se e positivo 
-   # sys.exit('Informe quantidade de elementos positivos da PG')
-#elif razaopg == 1:
-  #  print('A PG e  uma constante')
-#elif razaopg < 0 :
- #   print('A PG e uma Oscilante')
-#elif valorpg >= 1 < razaopg or 0<razaopg<1>valorpg<0:
-#    print('A PG e uma crescente')
-#elif 0<razaopg<1:
-#    print('A PG e  uma decrescente')
+#valorpa2=valorpa
+soma=vinicial
+#lista=' '
+if vquantidade<0:           #Verifica se e positivo 
+    sys.exit('Informe quantidade de elementos positivos da PG')
+elif razaopa == 0:
+    print('A PA e  uma constante')
+elif razaopa>0:    
+    print('A PA e uma crescente')
+elif razaopa<0:
+    print('A PA e  uma decrescente')
+print(f'o valor da PA({1})= {valorpa:,} ')
+for i in range (2,vquantidade+1):     
+    print(f'o valor da PA({i})={valorpa:,} + {razaopa} = {valorpa+razaopa}')
+    valorpa=vinicial+(i-1)*razaopa
+    soma+=valorpa
+    #lista+= str(valorpa2)
+    
 
-for i in range (1,vquantidade+1):     
-    valorpa2=valorpa+(i-1)*razaopa
-    lista+= str(valorpa2)
-    print(f'o valor da PA({i})= {valorpa2:,} ')
-#print(f'Os termos da PA são {lista}')
-#print(f'A soma dos valores dessa PA são: {valorpa:,}')
+print(f'A soma dos valores dessa PA são: {soma:,} ')
 
 #num=int(input('informe um outro valor inteiro que corresponde  a enezima posição dessa PG: '))
-#if num < 0:                       #Verifica se e positivo
  #   sys.exit('Informe uma quantidade de elementos positivos da PG')           
 #termoene= vinicial *(razaopg**(num-1))      #Calcula o valor de uma PG em uma enezima posição
 #print(f'o valor da enezima posição  {num} e {termoene:,}')
