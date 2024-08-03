355b556b2787c815b94c323f49cf36bd3aca6b2e
Yan F maia <31291355+yanmaia@users.noreply.github.com>
2024-08-03 03:03:09 -0300

    Update Questao2.py

--- Questao2.py
+++ Questao2.py
@@ -4,13 +4,13 @@ n2=int(input('Informe algum numero inteiro positivo: '))
 dividendo=n1
 divisor=n2
 resto= n1%n2
-if (n1 or n2) <= 0:
+if (n1 or n2) <= 0:                       
     sys.exit('informe um numero inteiro e positivo')
 elif n1 == n2:
     sys.exit(f'Os {n1} e {n2} possuem o mdc {n1}')
     
-while resto != 0:
-    dividendo=divisor
+while resto != 0:                  
+    dividendo=divisor 
     divisor=resto
-    resto= dividendo%divisor
+    resto= dividendo%divisor                                
 print(f"Os numeros {n1} e {n2}  possuem o mdc {divisor}")
