8c9cca5507e51694f6185ff18557f59c4ea1041d
Yan F maia <31291355+yanmaia@users.noreply.github.com>
2024-08-03 03:07:30 -0300

    Update and rename Questão7.py to 7.Questão.py

--- 7.Questão.py
+++ 7.Questão.py
@@ -3,9 +3,7 @@ vinicial=int(input('Informe um valor inteiro inicial da PA: '))
 razaopa=float(input('Informe a Razao da PA: '))
 vquantidade=int(input('Informe a quantidade de elementos positivo de uma  PA: '))
 valorpa=vinicial
-#valorpa2=valorpa
-soma=vinicial
-#lista=' '
+soma=vinicial 
 if vquantidade<0:           #Verifica se e positivo 
     sys.exit('Informe quantidade de elementos positivos da PG')
 elif razaopa == 0:
@@ -17,7 +15,7 @@ elif razaopa<0:
 print(f'o valor da PA({1})= {valorpa:,} ')
 for i in range (2,vquantidade+1):     
     print(f'o valor da PA({i})={valorpa:,} + {razaopa} = {valorpa+razaopa}')
-    valorpa=vinicial+(i-1)*razaopa
+    valorpa=vinicial+(i-1)*razaopa                   #Formula do calculo da PA
     soma+=valorpa
 print(f'A soma dos valores dessa PA são: {soma:,} ')
 num=int(input('informe um outro valor inteiro que corresponde  a enezima posição dessa PA: '))            
