93d07825fdc8a43c74ea12a22f98e4d490b89fad
Yan F maia <31291355+yanmaia@users.noreply.github.com>
2024-08-03 03:13:47 -0300

    Update 7.Questão.py

--- 7.Questão.py
+++ 7.Questão.py
@@ -12,12 +12,12 @@ elif razaopa>0:
     print('A PA e uma crescente')
 elif razaopa<0:
     print('A PA e  uma decrescente')
-print(f'o valor da PA({1})= {valorpa:,} ')
+print(f'o valor da PA({1})= {valorpa} ')
 for i in range (2,vquantidade+1):     
-    print(f'o valor da PA({i})={valorpa:,} + {razaopa} = {valorpa+razaopa}')
+    print(f'o valor da PA({i})={valorpa} + {razaopa} = {valorpa+razaopa}')
     valorpa=vinicial+(i-1)*razaopa                   #Formula do calculo da PA
     soma+=valorpa
-print(f'A soma dos valores dessa PA são: {soma:,} ')
+print(f'A soma dos valores dessa PA são: {soma:} ')
 num=int(input('informe um outro valor inteiro que corresponde  a enezima posição dessa PA: '))            
 termoene= vinicial +(num-1)*razaopa      #Calcula o valor de uma Pa em uma enezima posição
-print(f'o valor da enezima posição  {num} e {termoene:,}')
+print(f'o valor da enezima posição  {num} e {termoene}')
