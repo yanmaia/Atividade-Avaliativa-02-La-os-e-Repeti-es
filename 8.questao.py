93f0c2dabc6664a203f965afebdd02ee25d59be5
Yan F maia <31291355+yanmaia@users.noreply.github.com>
2024-08-03 18:02:19 -0300

    Update 8.questao.py

--- 8.questao.py
+++ 8.questao.py
@@ -5,11 +5,9 @@ numerotri=0
 if num<0:
     sys.exit("Informe um numero positivo para da certo o calculo")
 while  True:
-    numerotri=(contador/2) * (2*1+(contador-1)*1)
-    contador+=1
-    if numerotri == num:    
-        sys.exit("E um numero triangular")
-    if numerotri> num:
-        sys.exit('não e um numero triangular')  
-
-
+    numerotri=(contador/2) * (2*1+(contador-1)*1)                  #Calcula os numeros triangulares  pela formula
+    contador+=1                                                    
+    if numerotri == num:                                              #Checa se o numero informado pelo usuario corresponde a formula acima
+        sys.exit(f"{'-'*10} {num} E um numero triangular{'-'*10}") 
+    if numerotri> num:                                  
+        sys.exit(f"{'-'*10} {num} Não e um numero triangular{'-'*10}")  
