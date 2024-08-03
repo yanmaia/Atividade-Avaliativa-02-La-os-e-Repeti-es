f19da05c166ed7ad4207eca2e13314cc0aa986a2
Yan F maia <31291355+yanmaia@users.noreply.github.com>
2024-08-03 12:44:34 -0300

    Update 4.Questao.py

--- 4.Questao.py
+++ 4.Questao.py
@@ -1,11 +1,11 @@
+print('esses são os numeros menores que  1 milhão, múltipos de 2 ou 5 que podem ser escritos pela soma das potencias de 5 e de seus digitos')
 for i in range(1, 1000000):
     if i % 2 == 0 or i % 5 == 0:   #Comparação multp de 2 e 5
-        soma_potencias_5 = 0
+        somapotencia = 0
         num = i
         while num > 0:               #Decompõe o numero para  poder efetuar a soma 
             digito = num % 10                      
-            soma_potencias_5 += digito ** 5        #soma da potencia 5
+            somapotencia += digito ** 5        #soma da potencia 5
             num //= 10           
-        if soma_potencias_5 == i:                #Verifica se a soma da pot dos digitos e igual ao num
+        if somapotencia == i:                #Verifica se a soma da pot dos digitos e igual ao num
             print(i)
-print('esses são os numeros menores que  1 milhão, múltipos de 2 ou 5 que podem ser escritos pela soma das potencias de 5 e de seus digitos')
