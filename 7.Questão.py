bfbebf55685811fd1363ac5018a366e4d7c0e864
Yan F maia <31291355+yanmaia@users.noreply.github.com>
2024-08-03 17:59:20 -0300

    Update 7.Questão.py

--- 7.Questão.py
+++ 7.Questão.py
@@ -7,11 +7,11 @@ soma=vinicial
 if vquantidade<0:           #Verifica se e positivo 
     sys.exit('Informe quantidade de elementos positivos da PG')
 elif razaopa == 0:
-    print('A PA e  uma constante')
-elif razaopa>0:    
-    print('A PA e uma crescente')
-elif razaopa<0:
-    print('A PA e  uma decrescente')
+    print(f"{'-'*10}A PA e  uma constante{'-'*10}")
+elif razaopa>0:                                                                # #vai verificar as classificações das PA
+    print(f"{'-'*10}A PA e uma crescente{'-'*10}")
+elif razaopa<0: 
+    print(f"{'-'*10}A PA e  uma decrescente{'-'*10}")
 print(f'o valor da PA({1})= {valorpa} ')
 for i in range (2,vquantidade+1):     
     print(f'o valor da PA({i})={valorpa} + {razaopa} = {valorpa+razaopa}')
@@ -20,4 +20,4 @@ for i in range (2,vquantidade+1):
 print(f'A soma dos valores dessa PA são: {soma:} ')
 num=int(input('informe um outro valor inteiro que corresponde  a enezima posição dessa PA: '))            
 termoene= vinicial +(num-1)*razaopa      #Calcula o valor de uma Pa em uma enezima posição
-print(f'o valor da enezima posição  {num} e {termoene}')
+print(f"o valor da enezima posição  {num} e {termoene:,}")
